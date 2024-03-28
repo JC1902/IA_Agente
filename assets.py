e8aafccd84355c10ccf07bbef424252947da088c
Jorge Cisneros <jorgecisneros2002@gmail.com>
2024-03-27 20:16:42 -0600

    Practicamente todo lo visual terminado, a opinion personal

--- assets.py
+++ assets.py
@@ -4,20 +4,35 @@ import pygame
 CELL_SIZE = 80
 
 # Cargar las imágenes de los pisos
-imagen_0 = pygame.image.load('pisos/tierra.png')
-imagen_0 = pygame.transform.scale(imagen_0, (CELL_SIZE, CELL_SIZE))
-imagen_1 = pygame.image.load('pisos/pasto.png')
-imagen_1 = pygame.transform.scale(imagen_1, (CELL_SIZE, CELL_SIZE))
+tierra = pygame.image.load( 'pisos/tierra.png' )
+tierra = pygame.transform.scale( tierra, ( CELL_SIZE, CELL_SIZE ) )
+
+pasto = pygame.image.load( 'pisos/pasto.png' )
+pasto = pygame.transform.scale( pasto, ( CELL_SIZE, CELL_SIZE ) )
+
+lodo = pygame.image.load( 'pisos/lodo.png' )
+lodo = pygame.transform.scale( lodo, ( CELL_SIZE, CELL_SIZE ) )
+
+# Cargar las imagenes de las bayas para cargar la bateria
 bayas = [pygame.image.load( f'pisos/bayas_{i}.png' ) for i in range(3) ]
 bayas = [ pygame.transform.scale(imagen, (CELL_SIZE,CELL_SIZE)) for imagen in bayas ]
 
-# Cargar las imágenes de los obstáculos
-imagen_2 = pygame.image.load('obstaculos/arbol_0.png')
-imagen_2 = pygame.transform.scale(imagen_2, (CELL_SIZE, CELL_SIZE))
-#imagen_3 = pygame.image.load('obstaculos/sprite_45.png')
-#imagen_3 = pygame.transform.scale(imagen_3, (CELL_SIZE, CELL_SIZE))
+# Cargar las imágenes de los obstaculos
+arbol_pasto = pygame.image.load('obstaculos/arbol_0.png')
+arbol_pasto = pygame.transform.scale(arbol_pasto, (CELL_SIZE, CELL_SIZE))
+
+arbol_tierra = pygame.image.load('obstaculos/arbol_1.png')
+arbol_tierra = pygame.transform.scale(arbol_tierra, (CELL_SIZE, CELL_SIZE))
+
 snorlax_tierra = [ pygame.image.load( f'obstaculos/snorlax_{i}.png') for i in range(5) ]
 snorlax_tierra = [pygame.transform.scale(imagen, (CELL_SIZE, CELL_SIZE)) for imagen in snorlax_tierra]
+
+# Cargar las imagenes de los enemigos
+muk = [ pygame.image.load( f'enemigos/MUK_{i}.png' ) for i in range(3) ]
+#muk = [ pygame.transform.scale( imagen, ( CELL_SIZE / 2, CELL_SIZE / 2 ) ) for imagen in muk ]
+
+voltorb = [ pygame.image.load( f'enemigos/pozoV40_{i}.png' ) for i in range(6) ]
+#voltorb = [ pygame.transform.scale( imagen, ( CELL_SIZE / 2, CELL_SIZE / 2 ) ) for imagen in voltorb ]
 
 # Cargar las imágenes del personaje
 personaje_arriba = [pygame.image.load(f'pikachu/pikachu_arriba_{i}.png') for i in range(2)]
@@ -25,6 +40,7 @@ personaje_abajo = [pygame.image.load(f'pikachu/pikachu_abajo_{i}.png') for i in 
 personaje_izquierda = [pygame.image.load(f'pikachu/pikachu_izq_{i}.png') for i in range(2)]
 personaje_derecha = [pygame.image.load(f'pikachu/pikachu_der_{i}.png') for i in range(2)]
 
+# Asignar las imagenes a una direccion en especifico
 personajes = {
     "arriba": personaje_arriba,
     "abajo": personaje_abajo,
@@ -32,14 +48,17 @@ personajes = {
     "derecha": personaje_derecha
 }
 
+# Escalar al personaje a un tamaño adecuado
 personajes = {direccion: [pygame.transform.scale(img, (CELL_SIZE/1.5, CELL_SIZE/1.5)) for img in sprites] for direccion, sprites in personajes.items()}
 
+# Cargar los coleccionables
 coleccionables = [pygame.image.load(f'coleccionables/cc{i}.png') for i in range(10)]
 
 
-
+# Cargar la imagen para las vidas
 img_vidas = [pygame.image.load(f'interfaz/vida_{i}.png') for i in range(2)]
 
+# Cargar y acomodar el medidor de la bateria
 img_medidor = [
     pygame.transform.rotate(
         pygame.transform.scale(
--- main.py
+++ main.py
@@ -27,44 +27,32 @@ btn_start = Button( screen , 650, 800, 'Comenzar', 100 , 40 )
 # Matriz del juego
 mapaJuego = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 2, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 3, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 3, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 2, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 2, 2, 2, 2, 0, 1],
+    [1, 3, 2, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 2, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 2, 0, 0, 0, 2, 2, 3, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-
-# Posición inicial del personaje
-pos_personaje_x, pos_personaje_y = 1, 1
+# Posiciones posible para el personaje
+posiciones_personaje = [(1,1), (1,8), (8,1), (8,8)]
+pos_personaje_x, pos_personaje_y = random.choice(posiciones_personaje)
 
 # Dirección inicial del personaje
-direccion = "abajo"
+if (pos_personaje_x, pos_personaje_y) == (1,1) or (pos_personaje_x, pos_personaje_y) == (8,1):
+    direccion = "abajo"
+else:
+    direccion = "arriba"
 
 # Almacenar la posición anterior del personaje
 old_pos_personaje_x, old_pos_personaje_y = pos_personaje_x, pos_personaje_y
 
 # Fuera de la función colocar(), define un arreglo para las posiciones de los coleccionables
-posiciones_coleccionables = []
-
-# Llena el arreglo con posiciones aleatorias
-for _ in range( 10 ):  # Para 10 coleccionables
-    pos_x = random.randrange( 1, 9 )
-    pos_y = random.randrange( 1, 9 )
-    posiciones_coleccionables.append( ( pos_x, pos_y ) )
-
-"""def colocar(posiciones):
-    ancho_collec, alto_collect = CELL_SIZE // 1.5, CELL_SIZE // 1.5   
-
-    for i, (pos_x, pos_y) in enumerate(posiciones):
-        # Calcular la posición del coleccionable en la casilla actual
-        collec_pos_x = pos_x * CELL_SIZE + (CELL_SIZE - ancho_collec) 
-        collec_pos_y = pos_y * CELL_SIZE + (CELL_SIZE - alto_collect)
-        screen.blit(coleccionables[i], (collec_pos_x, collec_pos_y))"""
+posiciones_coleccionables = [(random.randrange(1, 9), random.randrange(1, 9)) for _ in range(10)]
 
 def colocar(posiciones):
     ancho_collec, alto_collect = CELL_SIZE // 1.5, CELL_SIZE // 1.5
@@ -72,18 +60,30 @@ def colocar(posiciones):
     i = 0
     while i < len(posiciones):
 
-        pos_x, pos_y = posiciones_coleccionables[i]
+        pos_x, pos_y = posiciones[i]
 
         # Verificar si la posición aleatoria coincide con la posición de las bayas o si el valor de la posición en la matriz es 3
-        if (pos_x, pos_y) != (4, 8) and mapaJuego[pos_y][pos_x] != 3:
+        if (pos_x, pos_y) != (4, 8) and \
+            mapaJuego[pos_y][pos_x] != 3 and \
+            (pos_x, pos_y) != (old_pos_personaje_x, old_pos_personaje_y) and \
+            mapaJuego[pos_y][pos_x] != 2:
             # Calcular la posición del coleccionable en la casilla actual
             collec_pos_x = pos_x * CELL_SIZE + (CELL_SIZE - ancho_collec) 
             collec_pos_y = pos_y * CELL_SIZE + (CELL_SIZE - alto_collect)
             screen.blit(coleccionables[i], (collec_pos_x, collec_pos_y))
             i += 1
         else:
-            pos_x = random.randrange( 1, 9 )
-            pos_y = random.randrange( 1, 9 )
+            while True:
+                pos_x = random.randrange(1, 9)
+                pos_y = random.randrange(1, 9)
+                if (pos_x, pos_y) not in posiciones:
+                    break
+            posiciones[i] = (pos_x, pos_y)
+
+            # Calcular la posición del coleccionable en la nueva casilla
+            collec_pos_x = pos_x * CELL_SIZE + (CELL_SIZE - ancho_collec) 
+            collec_pos_y = pos_y * CELL_SIZE + (CELL_SIZE - alto_collect)
+            screen.blit(coleccionables[i], (collec_pos_x, collec_pos_y))
 
 
 # Define el índice del frame actual de la animación de la imagen_3
@@ -98,16 +98,83 @@ def animate_bayas():
     global frame_index_bayas
     frame_index_bayas = ( frame_index_bayas + 1 ) % len(bayas)
 
+# Definir variables para controlar la animación de Muk
+frame_index_muk = 0
+muk_animation_counter = 0
+muk_animation_speed = 3  # Ajusta este valor para ralentizar la animación de Muk
+
+# Definir variables para controlar la animación de Voltorb
+frame_index_voltorb = 0
+voltorb_animation_counter = 0
+voltorb_animation_speed = 3  # Ajusta este valor para ralentizar la animación de Voltorb
+
+# Función para animar a Muk
+def animate_muk():
+    global frame_index_muk, muk_animation_counter
+
+    muk_animation_counter += 1
+    if muk_animation_counter >= muk_animation_speed:
+        frame_index_muk = (frame_index_muk + 1) % len(muk)
+        muk_animation_counter = 0
+
+# Función para animar a Voltorb
+def animate_voltorb():
+    global frame_index_voltorb, voltorb_animation_counter
+
+    voltorb_animation_counter += 1
+    if voltorb_animation_counter >= voltorb_animation_speed:
+        frame_index_voltorb = (frame_index_voltorb + 1) % len(voltorb)
+        voltorb_animation_counter = 0
+
+posiciones_muk = [(random.randrange(1, 9), random.randrange(1, 9)) for _ in range(4)]
+posiciones_voltorb = [(random.randrange(1, 9), random.randrange(1, 9)) for _ in range(4)]
+
+def colocar_enemigos(posiciones_enemigos, posiciones_colectables, frame_enemigo, enemigo_images):
+    ancho_enemigo, alto_enemigo = 40 , 40
+
+    if posiciones_enemigos == posiciones_muk:
+        posiciones_ocupadas = set(posiciones_colectables) | set(posiciones_voltorb)
+    elif posiciones_enemigos == posiciones_voltorb:
+        posiciones_ocupadas = set(posiciones_colectables) | set(posiciones_muk)
+
+    for i, (pos_x, pos_y) in enumerate(posiciones_enemigos):
+        while (pos_x, pos_y) in posiciones_ocupadas or \
+                mapaJuego[pos_y][pos_x] in [2, 3] or \
+                (pos_x, pos_y) == (4, 8) or \
+                (pos_x, pos_y) == (old_pos_personaje_x, old_pos_personaje_y):
+            pos_x = random.randrange(1, 9)
+            pos_y = random.randrange(1, 9)
+        
+        posiciones_enemigos[i] = (pos_x, pos_y)
+        posiciones_ocupadas.add((pos_x, pos_y))
+
+        enemigo_pos_x = pos_x * CELL_SIZE + (CELL_SIZE - ancho_enemigo) // 2
+        enemigo_pos_y = pos_y * CELL_SIZE + (CELL_SIZE - alto_enemigo) // 2
+        screen.blit(enemigo_images[frame_enemigo], (enemigo_pos_x, enemigo_pos_y))
+
+
+tipo_piso = []
+
+for y, row in enumerate(mapaJuego):
+        for x, cell in enumerate(row):
+            if cell == 0:
+                piso = random.choice( [ tierra, tierra, pasto, lodo ] )
+                tipo_piso.append(piso)
+
 # Función para dibujar el mapa del juego
 def draw_map(mapa):
+
+    index = 0
+
     for y, row in enumerate(mapa):
         for x, cell in enumerate(row):
             if cell == 0:
-                screen.blit(imagen_0, (x * CELL_SIZE, y * CELL_SIZE))
+                screen.blit( tipo_piso[index], (x * CELL_SIZE, y * CELL_SIZE))
+                index += 1
             elif cell == 1:
-                screen.blit(imagen_2, (x * CELL_SIZE, y * CELL_SIZE))
+                screen.blit(arbol_pasto, (x * CELL_SIZE, y * CELL_SIZE))
             elif cell == 2:
-                screen.blit(imagen_2, (x * CELL_SIZE, y * CELL_SIZE))
+                screen.blit(arbol_tierra, (x * CELL_SIZE, y * CELL_SIZE))
             elif cell == 3:
                 screen.blit(snorlax_tierra[frame_index_imagen_3], (x * CELL_SIZE, y * CELL_SIZE))
 
@@ -137,19 +204,19 @@ def main():
             if event.type == pygame.KEYDOWN:  # Manejar eventos de teclado
                 costo +=  1  if costo < BATERIA_MAX else 0
                 if event.key == pygame.K_UP:
-                    if pos_personaje_y > 0: 
+                    if pos_personaje_y > 0 and mapaJuego[pos_personaje_y - 1][pos_personaje_x] == 0: 
                         pos_personaje_y -= 1
                         direccion = "arriba"
                 elif event.key == pygame.K_DOWN:
-                    if pos_personaje_y < len(mapaJuego) - 1: 
+                    if pos_personaje_y < len(mapaJuego) - 1 and mapaJuego[pos_personaje_y + 1][pos_personaje_x] == 0: 
                         pos_personaje_y += 1
                         direccion = "abajo"
                 elif event.key == pygame.K_LEFT:
-                    if pos_personaje_x > 0: 
+                    if pos_personaje_x > 0 and mapaJuego[pos_personaje_y][pos_personaje_x - 1] == 0: 
                         pos_personaje_x -= 1
                         direccion = "izquierda"
                 elif event.key == pygame.K_RIGHT:
-                    if pos_personaje_x < len(mapaJuego[0]) - 1: 
+                    if pos_personaje_x < len(mapaJuego[0]) - 1 and mapaJuego[pos_personaje_y][pos_personaje_x + 1] == 0: 
                         pos_personaje_x += 1
                         direccion = "derecha"
 
@@ -173,6 +240,7 @@ def main():
         screen.fill(FONDO)
         draw_map(mapaJuego)
         colocar(posiciones_coleccionables)
+
         personaje_ancho, personaje_alto = CELL_SIZE // 1.5, CELL_SIZE // 1.5 
 
         #Boton y  evento 
@@ -199,10 +267,17 @@ def main():
             frame = (frame + 1) % 2  # This line switches the frame between 0 and 1
             animate_imagen_3()
             animate_bayas()
+            
+            animate_muk()
+            animate_voltorb()
+
+        colocar_enemigos( posiciones_muk, posiciones_coleccionables, frame_index_muk, muk )
+        colocar_enemigos( posiciones_voltorb, posiciones_coleccionables, frame_index_voltorb, voltorb )
 
         screen.blit( personajes[ direccion ][ frame ], ( personaje_pos_x, personaje_pos_y ) )
-        screen.blit( bayas[frame_index_bayas], (bayas_pos_x, bayas_pos_y) )
-        screen.blit(personajes[direccion][frame], (personaje_pos_x, personaje_pos_y))
+        
+        screen.blit( bayas[frame_index_bayas], (bayas_pos_x, bayas_pos_y) )        
+
         interfaz.dibujar_interface( screen , costo , vidas  )
        
         
